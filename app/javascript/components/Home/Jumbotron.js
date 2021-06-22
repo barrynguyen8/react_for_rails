35ccc7661215fcb0c03a2d2e684988689d2b19fd
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-06-22 15:22:30 +0000

    jumbotron react component added

---
+++ app/javascript/components/Home/Jumbotron.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+const Jumbotron = () => {
+    return (
+        <section className="home-section--1">
+            <div className="container">
+                <div className="row">
+                    <div className="col col-sm-12 col-md-5">
+                        <div className="pt-4 mt-4">
+                            <h1>React for Rails Developers</h1>
+                            <p>Supercharge your Ruby on Rails Apps with React.JS</p>
+                        </div>
+                    </div>
+                    <div className="col col-sm-12 col-md-7"></div> 
+                        <div className="pt-4 mt-4 text-center">
+                        
+                        </div>
+                </div>
+            </div>
+        </section> 
+        )
+}
+
+export default Jumbotron
